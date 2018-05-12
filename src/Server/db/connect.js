2bbe73a9ebc555246bc44e2a3cd71671762cd4f2
Deepak Mishra <deepakm157@gmail.com>
2018-05-12 14:10:56 +0530

    db config

--- src/Server/db/connect.js
+++ src/Server/db/connect.js
@@ -3,12 +3,7 @@
 var bookshelf = function (config) {
     var knex = require("knex")({
         client: "mysql",
-        connection: {
-            host: "127.0.0.1",
-            user: "root",
-            password: "",
-            database: "classtime"
-        }
+        connection: config.db
     });
     var bookshelf_v = require("bookshelf")(knex);
 
